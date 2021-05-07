5456a5e47d25366f95367b039b5d8958c287ec6d
parbilla <parbilla@gmail.com>
2021-05-07 20:45:50 +0000

    task 5

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,9 +2,26 @@
 """Create index"""
 from api.v1.views import app_views
 from flask import Flask, Blueprint, jsonify
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
 
 
 @app_views.route('/status')
 def status_url():
     """Method that returns a status JSON"""
     return jsonify({"status": "OK"})
+
+@app_views.route('/stats')
+def storage_stats():
+    """Endpoint that retrieves the number of each objects by type"""
+    return jsonify({"amenities": storage.count(Amenity),
+                    "cities": storage.count(City),
+                    "places": storage.count(Place),
+                    "reviews": storage.count(Review),
+                    "states": storage.count(State),
+                    "users": storage.count(User)})
