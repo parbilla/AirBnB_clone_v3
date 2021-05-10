a805ecb018cc9be84aa1b8725107f5988ee81712
nicolasportela <2103@holbertonschool.com>
2021-05-10 02:56:21 +0000

    task 11 fix

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -53,7 +53,8 @@ def postPlace(city_id=None):
 
     if (("City." + city_id) not in storage.all()):
         abort(404)
-    user = storage.get("User", request.get_json()['user_id'])
+    kwargs = request.get_json()
+    user = storage.get("User", kwargs['user_id']
     if user is None:
         abort(404)
     # if (("User." + user_id) not in storage.all()):
