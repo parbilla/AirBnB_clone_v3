2b612ea760984c42c5915001b1b3e2e50c7357db
nicolasportela <2103@holbertonschool.com>
2021-05-11 00:00:50 +0000

    storage engines edited

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -77,9 +77,7 @@ class DBStorage:
 
     def get(self, cls, id):
         """Returns the object based on the class and its ID"""
-        spl = str(cls).split(".")
-        spl2 = spl[2].split("'")
-        obj = spl2[0] + "." + id
+        obj = cls.__name__ + '.' + id
         if obj in self.all():
             return self.all().get(obj)
         else:
